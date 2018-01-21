c0999666adaa5029aa270d6576e509e1ba1bb100
Jonathan Porta <jonathan@jonathanporta.com>
2018-01-21 08:32:19 -0700

    :100: :rocket: Correct naming and bump version

--- recipes/autochef.rb
+++ recipes/autochef.rb
@@ -4,6 +4,16 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-package 'monitoring' do
-  action [:upgrade]
+bash 'ops_tools' do
+  code <<-EOH
+    dnf install -y jq https://github.com/JonathanPorta/ops/releases/download/0.0.4/ops-0.0.4-local.git860078f.x86_64.rpm
+    EOH
+  ignore_failure false
+end
+
+bash 'monitoring-cookbook' do
+  code <<-EOH
+    dnf install -y $(gh latest jonathanporta/monitoring-cookbook --download-url)
+    EOH
+  ignore_failure false
 end
